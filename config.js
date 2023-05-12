8f48633469c408b6d697fd07df95d8d7d4db6882
ishannimsaras <133310358+ishannimsaras@users.noreply.github.com>
2023-05-12 13:23:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['0764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94720179630', 'Owner', true],
+  ['94720179630', 'Ishan', true], 
   [''] 
 ] //Number of owners
 
